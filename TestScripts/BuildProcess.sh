5a43d2614143588c539cddf3f98af1c830cc86fd
Orod Razeghi <47788314+OrodRazeghi@users.noreply.github.com>
2020-06-02 21:55:01 +0100

    Update BuildProcess.sh

--- TestScripts/BuildProcess.sh
+++ TestScripts/BuildProcess.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
 #Prepare
-sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
-sudo apt-get update -qq
-sudo apt-get build-dep qt5-default
-sudo apt-get install libxcb-xinerama0-dev
+#sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
+#sudo apt-get update -qq
+#sudo apt-get build-dep qt5-default
+#sudo apt-get install libxcb-xinerama0-dev
 
 #Build QT
-git clone -branch 5.12 https://code.qt.io/qt/qt5.git
+git clone --branch 5.12 https://code.qt.io/qt/qt5.git
 cd qt5; git submodule update --init --recursive
 ../qt5/configure -developer-build -opensource -nomake examples -nomake tests
 make -j$(nproc)
 
+echo $PWD
 ls
 
 #Build MITK
 git clone --branch v2018.04.2 https://phabricator.mitk.org/source/mitk.git
 
-ls
 
 #mkdir Build; cd Build
 #cmake ../MITK
