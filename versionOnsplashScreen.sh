0b7563011f33343f84b40c812b827a69fbe630b4
Jose Alonso Solis-Lemus <alonso.jasl@gmail.com>
2021-06-10 18:57:39 +0100

    [minor] Version script updated

--- versionOnsplashScreen.sh
+++ versionOnsplashScreen.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 OUTFILE="./CemrgApp/Applications/MainApp/version.txt"
 
+# SHAVAR=$(git log --oneline -1 | awk -F ' ' '{print $1}')
+# VERVAR=$(git tag --points-at=${SHAVAR})
+#
+# echo $VERVAR
+# echo $SHAVAR
+#
+# echo $VERVAR > $OUTFILE
+# echo $SHAVAR >> $OUTFILE
+
 MYVAR=$(git tag)
 echo $MYVAR | awk -F v '{print $NF}' > $OUTFILE
 
