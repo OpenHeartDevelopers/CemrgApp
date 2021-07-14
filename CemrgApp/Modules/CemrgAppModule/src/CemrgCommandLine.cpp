97808a6e4874314f823fb49adee045d1669eb883
Onur Ülgen <onur.ulgen@kcl.ac.uk>
2021-07-14 11:56:50 +0100

    Fix a bug preventing image transformation for some images (CemrgCommandLine::ExecuteTransformation)

--- CemrgApp/Modules/CemrgAppModule/src/CemrgCommandLine.cpp
+++ CemrgApp/Modules/CemrgAppModule/src/CemrgCommandLine.cpp
@@ -302,7 +302,7 @@ void CemrgCommandLine::ExecuteTransformation(QString dir, QString imgname, QStri
         process->setWorkingDirectory(executablePath);
         arguments << imgNamefullpath; //input
         arguments << outAbsolutePath; //output
-        arguments << "-dof" << dofpath;
+        arguments << "-dofin" << dofpath;
         arguments << "-verbose" << "3";
 
     } else {
