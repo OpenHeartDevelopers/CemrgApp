6ac8132c1d65bf6f082bb5432da510245b0aea2f
Onur Ülgen <onur.ulgen@kcl.ac.uk>
2021-07-23 16:33:04 +0100

    Comment out the CemrgTests::flattening()
    Due to the removal of the MeshUtil class from the MITK, this function needs to be rewritten. Until then, it's been commented out.

--- CemrgApp/Modules/CemrgAppModule/src/CemrgTests.cpp
+++ CemrgApp/Modules/CemrgAppModule/src/CemrgTests.cpp
@@ -49,7 +49,6 @@ PURPOSE.  See the above copyright notices for more information.
 #include <vtkPointLocator.h>
 #include <mitkBoundingObjectCutter.h>
 #include <itkConformalFlatteningMeshFilter.h>
-#include <mitkMeshUtil.h>
 #include <vtkUnstructuredGridWriter.h>
 #include <vtkUnstructuredGridReader.h>
 #include <vtkExtractUnstructuredGrid.h>
@@ -321,6 +320,7 @@ void polygonCutter() {
     writer->Write();
 }
 
+/**** The MeshUtil class was removed from the MITK ****
 void flattening() {
 
     mitk::Surface::Pointer surface = mitk::IOUtil::Load<mitk::Surface>("/home/or15/Downloads/LGECART30MIN-Scar.vtk");
@@ -342,6 +342,7 @@ void flattening() {
     surface->SetVtkPolyData(MeshUtil<MeshType>::MeshToPolyData(mesh));
     mitk::IOUtil::Save(surface, "/home/or15/Downloads/test.vtk");
 }
+*/
 
 void RRcalcsAuto() {
 
