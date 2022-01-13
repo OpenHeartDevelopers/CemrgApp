98a4ef6f1c997640f6d07ea7b3a90d761ed44737
Jose Alonso Solis-Lemus <alonso.jasl@gmail.com>
2022-01-13 19:10:30 +0000

    [bugfix] manual - binarise image

--- CemrgApp/Modules/CemrgAppModule/src/CemrgAtrialTools.cpp
+++ CemrgApp/Modules/CemrgAppModule/src/CemrgAtrialTools.cpp
@@ -213,16 +213,8 @@ ImageType::Pointer CemrgAtrialTools::RemoveNoiseFromAutomaticSegmentation(QStrin
     keepObjs->SetAttribute(LabelShapeKeepNObjImgFilterType::LabelObjectType::NUMBER_OF_PIXELS);
     keepObjs->Update();
 
-    return keepObjs->GetOutput();
-}
-
-ImageType::Pointer CemrgAtrialTools::CleanAutomaticSegmentation(QString dir, QString segName, QString cleanName){
-    QString inputPath = dir + "/" + segName;
-    ImageType::Pointer orgSegImage = LoadImage(inputPath);
-    ImageType::Pointer atriumCoarse = RemoveNoiseFromAutomaticSegmentation(dir, segName);
-
-    // binarise to 1, 0
-    IteratorType imIter(atriumCoarse, atriumCoarse->GetLargestPossibleRegion());
+    ImageType::Pointer outIm = keepObjs->GetOutput();
+    IteratorType imIter(outIm, outIm->GetLargestPossibleRegion());
 
     imIter.GoToBegin();
     while(!imIter.IsAtEnd()){
@@ -231,6 +223,27 @@ ImageType::Pointer CemrgAtrialTools::CleanAutomaticSegmentation(QString dir, QSt
 
         ++imIter;
     }
+
+    return outIm;
+}
+
+void CemrgAtrialTools::QuickBinarise(ImageType::Pointer imToBin){
+    // binarise to 1, 0
+    IteratorType imIter(imToBin, imToBin->GetLargestPossibleRegion());
+
+    imIter.GoToBegin();
+    while(!imIter.IsAtEnd()){
+        float value = (imIter.Get() > 0) ? 1 : 0;
+        imIter.Set(value);
+
+        ++imIter;
+    }
+}
+
+ImageType::Pointer CemrgAtrialTools::CleanAutomaticSegmentation(QString dir, QString segName, QString cleanName){
+    QString inputPath = dir + "/" + segName;
+    ImageType::Pointer orgSegImage = LoadImage(inputPath);
+    ImageType::Pointer atriumCoarse = RemoveNoiseFromAutomaticSegmentation(dir, segName);
 
     if(!cleanName.isEmpty()){
         SaveImageToDisk(atriumCoarse, dir, cleanName);
